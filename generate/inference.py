e75ca6ae216d18e7c6f3d396238534fbce1cd45e
WRGaming <1649257558@qq.com>
2025-10-30 14:50:07 +0800

    代码生成任务微调

---
+++ generate/inference.py
@@ -0,0 +1,44 @@
+import torch
+from transformers import pipeline
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+pipe = pipeline("text-generation", model="codeparrot-ds/checkpoint-65243", device=device)
+
+# test case 1: scatter plot
+txt = """\
+# create some data
+x = np.random.randn(100)
+y = np.random.randn(100)
+
+# create scatter plot with x, y
+"""
+print(pipe(txt, num_return_sequences=1)[0]["generated_text"])
+
+# test case 2: pandas
+txt = """\
+# create some data
+x = np.random.randn(100)
+y = np.random.randn(100)
+
+# create dataframe from x and y
+"""
+print(pipe(txt, num_return_sequences=1)[0]["generated_text"])
+
+# test case 3: pandas
+txt = """\
+# dataframe with profession, income and name
+df = pd.DataFrame({'profession': x, 'income':y, 'name': z})
+
+# calculate the mean income per profession
+"""
+print(pipe(txt, num_return_sequences=1)[0]["generated_text"])
+
+# test case 4: sklearn
+txt = """
+# import random forest regressor from scikit-learn
+from sklearn.ensemble import RandomForestRegressor
+
+# fit random forest model with 300 estimators on X, y:
+"""
+print(pipe(txt, num_return_sequences=1)[0]["generated_text"])
